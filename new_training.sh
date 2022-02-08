54972858e9105b2f540be76079805db0f93a0ae8
profenpoche <contact@profenpoche.com>
2022-02-08 11:17:42 +0100

    add files

---
+++ new_training.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# ============================================================================
+# This script is used for every new training of DeepSpeech
+# ============================================================================
+
+
+print_help () {
+	echo "new_training.sh : Create a new repo with all resources needed by DeepSpeech for a training"
+	echo "Usage : ./new_training.sh [OPTIONAL: repo_name]"
+}
+
+
+# Check if argument is present
+if [ $# == 0 ]; then
+    echo "No args, the repo name will be new_training";
+    filename="new_training"	
+else
+    filename=$1
+fi
+
+# Check if help argument is present
+if [ $1 == '-h' ]; then
+	print_help
+        exit 0
+fi
+
+echo "Creating repos and moving files in" $filename
+mkdir ../$filename
+mkdir ../$filename/checkpoints ../$filename/consommation ../$filename/exported-model
